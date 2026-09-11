f993bf78ac7b842ef2b0a31fefa95d9714670e56
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2026-09-11 11:03:06 +0200

    Missing changes from last commit

--- Lektion01/Opgave05/Program.cs
+++ Lektion01/Opgave05/Program.cs
@@ -12,11 +12,13 @@ namespace Opgave05
                         var r1 = new GeoPointRecord(56.15, 10.20);
                         var r2 = new GeoPointRecord(56.15, 10.20);
 
+                        // Both equate to false for some reason
                         Console.WriteLine(ReferenceEquals(c1, c2));
                         Console.WriteLine(ReferenceEquals(r1, r2));
 
                         Console.WriteLine();
 
+                        // Writes out the name of the object type
                         Console.WriteLine(c1.ToString());
                         Console.WriteLine(r1.ToString());
 
