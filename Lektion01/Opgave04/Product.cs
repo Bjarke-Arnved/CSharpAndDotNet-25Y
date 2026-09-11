b6c0699fda512d55d6177307932b4077a08cc421
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2026-09-11 10:34:18 +0200

    Added my solution to exercise 4 in Lesson01

---
+++ Lektion01/Opgave04/Product.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave04
+{
+        public record Product(string Id, string Name, decimal Price, string Category);
+}
---
+++ Lektion01/Opgave04/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Opgave04
+{
+        internal class Program
+        {
+                static void Main(string[] args)
+                {
+                        Product computer = new("1531", "Computer", 5000, "Hardware");
+                        Product computerMus = new("1507", "Computer Mus", 500, "Hardware");
+                        var discounted = computerMus with { Price = 399m };
+                        Console.WriteLine("Discount:");
+                        Console.WriteLine(discounted);
+                        Console.WriteLine("Orginal pris:");
+                        Console.WriteLine(computerMus);
+
+                        var (id, name, price, category) = computer;
+                        Console.WriteLine($"Vare: {name}, Pris: {price}");
+
+                }
+        }
+}
