386de3bf5971f1396de92c29b9d511db2387c79b
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2026-09-11 10:21:11 +0200

    My solution to Exercise 3 in Lesson01

---
+++ Lektion01/Opgave03/Program.cs
@@ -0,0 +1,28 @@
+﻿using Opgave03.model;
+
+namespace Opgave03
+{
+        public class Program
+        {
+                static void Main(string[] args)
+                {
+                        Console.WriteLine("Testing the Account class");
+                        BankAccount randomAccount = new BankAccount("DSGEE325215", "Albus Percival Wulfric Brian Dumbledore", 0);
+                        Console.WriteLine("Getting values from properties: ");
+                        Console.WriteLine("Account number: " + randomAccount.AccountNumber);
+                        Console.WriteLine("Owner: " + randomAccount.Owner);
+                        Console.WriteLine("Balance: " + randomAccount.Balance);
+                        Console.WriteLine("Is overdrawn: " + randomAccount.IsOverdrawn);
+                        Console.WriteLine("Formatted balance: " + randomAccount.FormattedBalance);
+                        Console.WriteLine("Methods: ");
+                        Console.WriteLine("Depositing 200 DKK");
+                        randomAccount.Deposit(200);
+                        Console.WriteLine(randomAccount.FormattedBalance);
+                        Console.WriteLine("Withdrawing 400 DKK");
+                        randomAccount.Withdraw(400);
+                        Console.WriteLine(randomAccount.FormattedBalance);
+                        Console.WriteLine("Is overdrawn: " + randomAccount.IsOverdrawn);
+
+                }
+        }
+}
---
+++ Lektion01/Opgave03/model/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave03.model
+{
+        public class BankAccount
+        {
+                public BankAccount(string accountNumber, string owner, decimal balance)
+                {
+                        AccountNumber = accountNumber;
+                        Owner = owner;
+                        Balance = balance;
+                }
+
+                public string AccountNumber { get; init; }
+                public string Owner { 
+                        get;
+                        set 
+                        {
+                                if (value == null || value == "") throw new ArgumentException("Input is null or empty");
+                                else field = value;       
+                        } 
+                }
+                public decimal Balance { get; private set; }
+                public bool IsOverdrawn { get => Balance < 0; }
+                public string FormattedBalance { get => $"Your account balance is: {Balance} DKK"; }
+                
+                public void Deposit(decimal amount)
+                {
+                        Balance += amount;
+                }
+                public void Withdraw(decimal amount)
+                {
+                        Balance -= amount;
+                }
+        }
+}
