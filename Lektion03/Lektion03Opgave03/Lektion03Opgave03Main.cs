3bb777cd93849a83d722d17dec5004c487e9ec99
Bjarke Arnved <eaa23bjar@students.eaaa.dk>
2026-09-13 13:49:11 +0200

    Solutions to 1, 2, 3 and 4 in Exercies03 Lesson03

--- Lektion03/Lektion03Opgave03/Lektion03Opgave03Main.cs
+++ Lektion03/Lektion03Opgave03/Lektion03Opgave03Main.cs
@@ -2,8 +2,20 @@ namespace Lektion03Opgave03;
 
 class Lektion03Opgave03Main
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         // TODO: Gør Main asynkron (async Task Main) og kalde UserRepository herfra med await
+        StarWarsRepository repos = new();
+
+        Console.WriteLine("Før kald til reposetory");
+        string result1 = await repos.GetUserByIdAsync(1);
+        string result2 = await repos.GetUserByIdAsync(2);
+        string result3 = await repos.GetUserByIdAsync(5);
+        Console.WriteLine("Efter kald til reposetory");
+
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(result3);
+        
     }
 }
