8c0c6ecc529a7c76be56714892d6b141640578e4
Bjarke Arnved <eaa23bjar@students.eaaa.dk>
2026-09-13 10:57:10 +0200

    Added my solution to Exercise03 Lesson02

---
+++ Lektion02/Opgave03/Opgave03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Opgave03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Dette er et leetspeak eksempel";
+            Console.WriteLine(text.ToLeetSpeak());
+        }
+    }
+}
---
+++ Lektion02/Opgave03/Opgave03/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave03
+{
+    public static class StringExtensions
+    {
+        public static string ToLeetSpeak(this string str)
+        {
+            str.ToLower();
+            StringBuilder leetStringBuilder = new(str);
+                leetStringBuilder.Replace('a', '4');
+                leetStringBuilder.Replace('e', '3');
+                leetStringBuilder.Replace('i', '1');
+                leetStringBuilder.Replace('l', '1');
+                leetStringBuilder.Replace('o', '0');
+                leetStringBuilder.Replace('s', '5');
+                leetStringBuilder.Replace('t', '7');
+            return leetStringBuilder.ToString();
+        }
+    }
+}
