401c222816a343a31730cce7c00c27a7e7c29c77
Bjarke Arnved <eaa23bjar@students.eaaa.dk>
2026-09-13 11:14:20 +0200

    My Solution to Exercies01 Lesson03

--- Lektion03/Lektion03Opgave01/Lektion03Opgave01Main.cs
+++ Lektion03/Lektion03Opgave01/Lektion03Opgave01Main.cs
@@ -1,9 +1,33 @@
 ﻿namespace Lektion03Opgave01;
 
-class Lektion03Opgave01Main
+public class Lektion03Opgave01Main
 {
-    static void Main(string[] args)
+    public delegate double MathOperation(double x, double y);
+    public static void Main(string[] args)
     {
-        
+        ExecuteAndPrint(5, 6, Add);
+        ExecuteAndPrint(5, 6, Subtract);
+        ExecuteAndPrint(5, 6, Multiply);
+        ExecuteAndPrint(50, 6, (x, y) => x / y);
+        ExecuteAndPrint(5, 6, (x, y) => Math.Pow(x, y));
+
+
+
+    }
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+    public static double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+    public static void ExecuteAndPrint(double x, double y, MathOperation operation)
+    {
+        Console.WriteLine(operation(x, y));
     }
 }
