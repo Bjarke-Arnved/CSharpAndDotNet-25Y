fa67d99fb11cb6656d10b97f655a92f29ab2c2a5
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2026-09-15 10:46:14 +0200

    Solution to Lesson 3 Exercise 3

---
+++ Lektion03/Lektion03Opgave04/models/CatFactDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lektion03Opgave04.models
+{
+        public record CatFactDTO(string fact, int length);
+        
+}
