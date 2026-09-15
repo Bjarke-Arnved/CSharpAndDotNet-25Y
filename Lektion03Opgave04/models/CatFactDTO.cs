7b0f298a7ec14f230f6ffc678e2e55e38d2ce307
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2026-09-15 10:47:53 +0200

    Deleted duplicate folder

--- Lektion03Opgave04/models/CatFactDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lektion03Opgave04.models
-{
-        public record CatFactDTO(string fact, int length);
-        
-}
